d0362fb796a6c779910150fb8c7dee58af08e5d0
Aaron Santos <2302042@utrivieramaya.edu.mx>
2026-07-11 18:28:01 -0500

    chore: adjust ESLint security rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,42 @@
 import js from '@eslint/js'
 import pluginSecurity from 'eslint-plugin-security'
+import vueParser from 'vue-eslint-parser'
 import globals from 'globals'
 
 export default [
   js.configs.recommended,
   pluginSecurity.configs.recommended,
   {
+    files: ['src/**/*.js'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.es2021
       }
     },
-    files: ['src/**/*.js', 'src/**/*.vue'],
     rules: {
-      // Reglas de seguridad activas
+      'security/detect-object-injection': 'warn',
+      'security/detect-non-literal-regexp': 'warn',
+      'security/detect-unsafe-regex': 'error',
+      'security/detect-possible-timing-attacks': 'warn',
+      'no-eval': 'error',
+      'no-implied-eval': 'error',
+      'no-new-func': 'error'
+    }
+  },
+  {
+    files: ['src/**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      }
+    },
+    plugins: {
+      security: pluginSecurity
+    },
+    rules: {
       'security/detect-object-injection': 'warn',
       'security/detect-non-literal-regexp': 'warn',
       'security/detect-unsafe-regex': 'error',
